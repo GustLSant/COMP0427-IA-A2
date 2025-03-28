a9afb79f65cdce65a892516ceeb75d957a1aebe2
Gustavo LS <gustlsantdev@gmail.com>
2025-03-28 18:22:06 -0300

    added messages array

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,33 @@
 import { useState, useRef } from "react";
 import { BiMenu, BiSend } from "react-icons/bi";
 
-let canSendMessage: boolean = false;
+
+type Message = {
+  sender: 'user' | 'ai';
+  data: string,
+}
+
+const defaultMessages: Message[] = [
+  {
+    sender: 'user',
+    data: 'Quem é o cabra mais inteligente do mundo?'
+  },
+  {
+    sender: 'ai',
+    data: 'Você mesmo!'
+  }
+]
 
 
 export default function App(){
   const [textInput, setTextInput] = useState<string>('');
+  const [messages, setMessages] = useState<Message[]>(defaultMessages);
   const textAreaRef = useRef<HTMLTextAreaElement | null>(null);
 
 
   function handleTextAreaChange(event: React.ChangeEvent<HTMLTextAreaElement>){
     setTextInput(event.target.value);
-    
+
     if(textAreaRef.current){
       textAreaRef.current.style.height = 'auto';
       textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`; // Ajusta a altura
@@ -20,18 +36,32 @@ export default function App(){
 
 
   function handleTextAreaKeyDown(event: React.KeyboardEvent<HTMLTextAreaElement>){
-    if(event.key === 'Enter'){
-      if(canSendMessage){ // duplo pressionamento do Enter, envio da mensagem
-        event.preventDefault();
-        canSendMessage = false;
-      }
-      else{ // quebra da primeira linha
-        console.log('quebra da primeira linha');
-        canSendMessage = true;
-      }
+    if(event.key === 'Enter' && !event.shiftKey){
+      event.preventDefault();
+      sendMessageToAi();
     }
-    else{ // digitacao comum
-      canSendMessage = false;
+    else{ // quebra de linha comum
+      console.log('quebra de linha');
+    }
+  }
+
+
+  function sendMessageToAi(){
+    const newMessages: Message[] = [...messages];
+    newMessages.push({
+      sender: 'user',
+      data: textInput
+    })
+    newMessages.push({
+      sender: 'ai',
+      data: 'Resposta da IA'
+    })
+    setMessages(newMessages);
+    setTextInput('');
+
+    // reset na altura do input
+    if(textAreaRef.current){
+      textAreaRef.current.style.height = 'auto';
     }
   }
 
@@ -44,10 +74,25 @@ export default function App(){
       </header>
 
       <div className="p-4 pt-0 grow flex flex-col gap-4 overflow-hidden">
-        <main className="grow overflow-auto max-w-[80vw] self-center pr-2">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas illo, repellat, eos nihil deleniti explicabo vel consectetur, laboriosam temporibus corrupti aliquid maiores inventore maxime fugiat sit saepe quia ipsam consequatur?</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas illo, repellat, eos nihil deleniti explicabo vel consectetur, laboriosam temporibus corrupti aliquid maiores inventore maxime fugiat sit saepe quia ipsam consequatur?</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas illo, repellat, eos nihil deleniti explicabo vel consectetur, laboriosam temporibus corrupti aliquid maiores inventore maxime fugiat sit saepe quia ipsam consequatur?</p>
+        <main className="grow overflow-auto max-w-[80vw] self-center pr-2 flex flex-col gap-4">
+          {
+            messages.map((message: Message)=>{
+              if(message.sender === 'ai'){
+                return(
+                  <div className="p-2 rounded-sm bg-blue-500">
+                    <p className="whitespace-pre-line">{message.data}</p>
+                  </div>
+                )
+              }
+              else{
+                return(
+                  <div className="self-end p-2 rounded-sm rounded-tr-none bg-red-500">
+                    <p className="whitespace-pre-line">{message.data}</p>
+                  </div>
+                )
+              }
+            })
+          }
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas illo, repellat, eos nihil deleniti explicabo vel consectetur, laboriosam temporibus corrupti aliquid maiores inventore maxime fugiat sit saepe quia ipsam consequatur?</p>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas illo, repellat, eos nihil deleniti explicabo vel consectetur, laboriosam temporibus corrupti aliquid maiores inventore maxime fugiat sit saepe quia ipsam consequatur?</p>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas illo, repellat, eos nihil deleniti explicabo vel consectetur, laboriosam temporibus corrupti aliquid maiores inventore maxime fugiat sit saepe quia ipsam consequatur?</p>
